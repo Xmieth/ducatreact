18c79253e255464adeb252fa5b5bccd03d642bbb
Xmieth <94183896+Xmieth@users.noreply.github.com>
2023-02-12 11:11:10 +0530

    This is for knowledge purpose

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React , {Fragment} from 'react';
+import './style.css' ;
+//Myhome ka M capital due to (first letter capital of function and components same in React)
+
+const Myhome = () => {
+    return "hiiii";
+}
+
+export const Myhome1 = ()=>{
+    return <h1 className='first'>Myhome is here</h1> ;
+}
+
+export const Myhome2 = ()=>{
+    return(
+    <Fragment>
+        <h1>lkdfsjgijdfli</h1>
+        <h2>sitting</h2>
+    </Fragment>
+    );
+}
+export default Myhome
